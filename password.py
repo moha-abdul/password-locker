f0da3489d1e118ca0fc7d476e8e993a636581903
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-23 19:40:23 +0300

    create init method to define properties in object

--- password.py
+++ password.py
@@ -1,2 +1,11 @@
-class User_data(self, first_name, last_name, phone_number, age, gender):
-    
+class User_data:
+    user_info = []
+    '''
+    this class collects user data
+    '''
+    def __init__(self, first_name, last_name, phone_number, age, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.age = age
+        self.gender = gender
