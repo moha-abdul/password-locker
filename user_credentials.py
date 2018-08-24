d64850aab3f3e9fd17a2a510d1f4b078bae2f32b
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-24 12:11:44 +0300

    init method for new user data

--- user_credentials.py
+++ user_credentials.py
@@ -1,11 +1,16 @@
 class User_credentials:
-    user_info = []
+    user_credentials = []
     '''
     this class creates  new instance of user data
     '''
-    def __init__(self, first_name, last_name, phone_number, age, gender):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
-        self.age = age
-        self.gender = gender
+    def __init__(self, username, password, account_type):
+        '''
+        init method to define objects
+        Args: 
+            username: to ask user a username
+            password: to either generate or ask user for a password
+            account_type: type of account like twitter, facebook
+        '''
+        self.username = username
+        self.password = password
+        self.account_type = account_type
